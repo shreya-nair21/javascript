be81f1271efb1ff2c7bee8b4076e17633c3518d9
shreya-nair21 <shreyanair2104@gmail.com>
2025-09-15 14:37:30 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -4,3 +4,20 @@ console.log(typeof (score))
 
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber)
+
+
+
+
+//"33" => 33
+// "33abc" => NaN (not a number)
+// true => 1 ; false => 0; "" => false; "shreya" => true
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
