c20fe31585e0e80d8f7772a783e1c4adb6ffdf1c
shreya-nair21 <shreyanair2104@gmail.com>
2025-09-16 13:50:52 +0000

    Converison operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "33"
+/*let score = "33"
 
 console.log(typeof (score))
 
@@ -21,3 +21,39 @@ console.log(booleanIsLoggedIn)
 let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
+*/
+
+//*********************OPERATIONS***********************//
+
+/*let value = 3
+let  negValue = -value
+console.log(negValue)*/
+
+/*console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)   //power
+console.log(2/2)
+console.log(2%2)*/
+
+/*let str1 = "hello"
+let str2 = " shreya"
+let str3 = str1 + str2
+console.log(str3)*/
+
+
+/*console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)  // if string is 1st then everyone is treated as string
+console.log(1 + 2 + "2") // if string is last then first elements' conversion is done
+
+console.log((3+4) * 5 % 3)*/ //write code like this not like abiove. use parantheses
+
+
+console.log(true);
+console.log(+true);
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,27 @@
+//console.log(2 > 1)
+//console.log(2 >= 1)
+//console.log(2 < 1)
+//console.log(2 == 1)
+//console.log(2 != 1)
+
+
+console.log("2" > 1)
+console.log("02" > 1)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined > 0)
+
+
+
+//comparison and equality checks work differently
+//comparison converts null to number , treating it as 0
+
+
+// === (strict check checks datatype as well)
+
+console.log("2" === 2)
