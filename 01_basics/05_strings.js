8ff667cf11a302bf83d9ab8da0e0452ee7b016bd
shreya-nair21 <shreyanair2104@gmail.com>
2025-09-19 09:23:07 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,24 @@
+const name = "shreya"
+const repoCount = 50
+
+
+console.log(name + repoCount + "value"); //outdated
+
+//string interpolation
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String('shr-eya')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('h'));
+
+const newString = gameName.substring(0, 4);
+console.log(newString)
+
+
+const newString1= gameName.slice(-5, 4);
+console.log(newString1)
+ 
